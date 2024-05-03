eb1a5d21d8e97fd706a5739bee7cb98f1b7601a5
jsepucas <jsepucas@myuax.com>
2024-05-03 18:07:28 +0200

    Commits

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,2 +1,8 @@
+import D_Autenticacion.VentanaInicioSesion;
+
 public class Principal {
+    public static void main(String[] args) {
+        VentanaInicioSesion ventanaInicioSesion = new VentanaInicioSesion();
+        ventanaInicioSesion.setVisible(true);
+    }
 }
