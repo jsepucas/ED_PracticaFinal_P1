56fdffbdd7c93575a6289f7780341a212ec3b237
jsepucas <jsepucas@myuax.com>
2024-05-03 20:13:20 +0200

    Commits

--- src/main/java/B_Gestion_Datos/AdministradorDatos.java
+++ src/main/java/B_Gestion_Datos/AdministradorDatos.java
@@ -1,4 +1,12 @@
 package B_Gestion_Datos;
 
+import A_Interfaz_Usuario.Experimento;
+import java.io.*;
+
 public class AdministradorDatos {
-}
+
+    public static Experimento abrirExperimento(File archivo) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(archivo))) {
+            return (Experimento) ois.readObject();
+        }
+    }
