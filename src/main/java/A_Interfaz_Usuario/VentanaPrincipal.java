2cc9aebae286e3b8c9b84e1b9de0079052c645a6
jsepucas <jsepucas@myuax.com>
2024-05-03 19:23:59 +0200

    Commits

--- src/main/java/A_Interfaz_Usuario/VentanaPrincipal.java
+++ src/main/java/A_Interfaz_Usuario/VentanaPrincipal.java
@@ -2,7 +2,11 @@ package A_Interfaz_Usuario;
 
 import javax.swing.*;
 import java.awt.*;
-public class VentanaPrincipal {
-    public void setVisible(boolean b) {
+public class VentanaPrincipal extends JFrame {
+    public VentanaPrincipal() {
+        setTitle("Sistema de Gestión de Experimentos");
+        setSize(1200, 800);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        initUI();
     }
-}
