cada7f18bfede439e144e1ad1b592f21588098a5
jsepucas <jsepucas@myuax.com>
2024-05-03 18:23:26 +0200

    Commits

--- src/main/java/D_Autenticacion/VentanaInicioSesion.java
+++ src/main/java/D_Autenticacion/VentanaInicioSesion.java
@@ -7,4 +7,14 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+public class VentanaInicioSesion extends JFrame implements ActionListener {
+    private JTextField textFieldUsuario;
+    private JPasswordField passwordField;
+    private JButton botonIniciarSesion;
 
+    public VentanaInicioSesion() {
+        super("Inicio de Sesión");
+        this.setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 600); // Aumenta el tamaño de la ventana
+        this.setLocationRelativeTo(null); // Centrar en pantalla
