457b56ced1ed93b152f86803097a3069efab095c
jsepucas <jsepucas@myuax.com>
2024-05-04 18:25:35 +0200

    Commitss

--- src/main/java/A_Interfaz_Usuario/VentanaPrincipal.java
+++ src/main/java/A_Interfaz_Usuario/VentanaPrincipal.java
@@ -4,8 +4,8 @@ import B_Gestion_Datos.AdministradorDatos;
 import B_Gestion_Datos.Experimento;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 import java.io.*;
+import java.net.URL;
 
 public class VentanaPrincipal extends JFrame {
     private Experimento experimentoActual; // Mantiene el experimento actual
@@ -24,7 +24,25 @@ public class VentanaPrincipal extends JFrame {
     private void initUI() {
         // Menú principal
         JMenuBar menuBar = new JMenuBar();
-        JMenu menuArchivo = new JMenu("Archivo");
+
+        // Crear un JMenu con un icono
+        JMenu menuIcono = new JMenu();
+        menuIcono.setIcon(new ImageIcon(getClass().getResource("/images/icono.png")));
+
+
+        URL imageUrl = getClass().getResource("/images/icono.png");
+        if (imageUrl != null) {
+            ImageIcon imageIcon = new ImageIcon(new ImageIcon(imageUrl).getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
+            menuIcono.setIcon(imageIcon);
+            menuIcono.revalidate();
+            menuIcono.repaint();
+        } else {
+            System.out.println("Image not found");
+        }
+
+
+        // Crear los JMenuItem para el menú desplegable
+        JMenuItem itemArchivo = new JMenuItem("Archivo");
         JMenuItem itemNuevo = new JMenuItem("Nuevo Experimento");
         JMenuItem itemAbrir = new JMenuItem("Abrir Experimento");
         JMenuItem itemGuardar = new JMenuItem("Guardar");
@@ -39,16 +57,34 @@ public class VentanaPrincipal extends JFrame {
         itemEditar.addActionListener(e -> editarExperimento());
         itemEliminar.addActionListener(e -> eliminarExperimento());
 
-        menuArchivo.add(itemNuevo);
-        menuArchivo.add(itemAbrir);
-        menuArchivo.add(itemGuardar);
-        menuArchivo.add(itemEditar);
-        menuArchivo.add(itemEliminar);
-        menuArchivo.add(new JSeparator());
-        menuArchivo.add(itemSalir);
-        menuBar.add(menuArchivo);
+        // Agregar los JMenuItem al JMenu
+        menuIcono.add(itemArchivo);
+        menuIcono.add(itemNuevo);
+        menuIcono.add(itemAbrir);
+        menuIcono.add(itemGuardar);
+        menuIcono.add(itemEditar);
+        menuIcono.add(itemEliminar);
+        menuIcono.add(new JSeparator());
+        menuIcono.add(itemSalir);
+
+        // Agregar el JMenu al menuBar
+        menuBar.add(menuIcono);
         setJMenuBar(menuBar);
 
+        URL exitIconUrl = getClass().getResource("/images/exiticon.png");
+        if (exitIconUrl != null) {
+            ImageIcon exitIcon = new ImageIcon(new ImageIcon(exitIconUrl).getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
+            JMenuItem exitItem = new JMenuItem(exitIcon);
+
+            // Add an ActionListener to the JMenuItem
+            exitItem.addActionListener(e -> System.exit(0));
+
+            // Add the JMenuItem to the right of the JMenuBar
+            menuBar.add(Box.createHorizontalGlue());
+            menuBar.add(exitItem);
+        } else {
+            System.out.println("Exit icon not found");
+        }
 
 
         // Panel de visualización
@@ -57,13 +93,7 @@ public class VentanaPrincipal extends JFrame {
         labelBienvenida = new JLabel("Bienvenido al Sistema de Gestión de Experimentos de la UAX", SwingConstants.CENTER);
         panelCentral.add(labelBienvenida, BorderLayout.CENTER);
         add(panelCentral);
-
-
-
-
-
     }
-
     private void eliminarExperimento() {
 
         if (experimentoActual != null) {
