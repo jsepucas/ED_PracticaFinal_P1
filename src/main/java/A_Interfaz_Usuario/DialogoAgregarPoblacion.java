0a18775002d1131e41b18bf5a63caec1e2b2fc2e
jsepucas <jsepucas@myuax.com>
2024-05-04 14:40:52 +0200

    Commits

--- src/main/java/A_Interfaz_Usuario/DialogoAgregarPoblacion.java
+++ src/main/java/A_Interfaz_Usuario/DialogoAgregarPoblacion.java
@@ -1,4 +1,9 @@
 package A_Interfaz_Usuario;
 
-public class DialogoAgregarPoblacion {
-}
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class DialogoAgregarPoblacion extends JDialog {
+    private JTextField nombrePoblacionField;
+    private JButton guardarBtn, cancelarBtn;
