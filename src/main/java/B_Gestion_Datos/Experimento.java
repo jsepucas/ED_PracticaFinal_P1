2b746c5a7bbe64532915509b932df38c5a35e822
jsepucas <jsepucas@myuax.com>
2024-05-04 20:22:56 +0200

    Commitss

--- src/main/java/B_Gestion_Datos/Experimento.java
+++ src/main/java/B_Gestion_Datos/Experimento.java
@@ -1,84 +1,30 @@
 package B_Gestion_Datos;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Experimento {
     private String nombre;
-    private String bacteria;
+    private List<PoblacionBacterias> poblaciones;
     private Date fechaInicio;
     private Date fechaFin;
     private int numeroInicialBacterias;
     private double temperatura;
     private String condicionLuz;
     private double dosisComida;
-    private int[] dosisDiaria;
 
-    // Add getters and setters for the new fields
-    public String getNombre() {
-        return nombre;
+    public Experimento() {
+        this.poblaciones = new ArrayList<>();
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    // Getters and setters
+
+    public void addPoblacion(PoblacionBacterias poblacion) {
+        this.poblaciones.add(poblacion);
     }
 
-    public String getBacteria() {
-        return bacteria;
-    }
-
-    public void setBacteria(String bacteria) {
-        this.bacteria = bacteria;
-    }
-
-    public Date getFechaInicio() {
-        return fechaInicio;
-    }
-
-    public void setFechaInicio(Date fechaInicio) {
-        this.fechaInicio = fechaInicio;
-    }
-
-    public Date getFechaFin() {
-        return fechaFin;
-    }
-
-    public void setFechaFin(Date fechaFin) {
-        this.fechaFin = fechaFin;
-    }
-
-    public int getNumeroInicialBacterias() {
-        return numeroInicialBacterias;
-    }
-
-    public void setNumeroInicialBacterias(int numeroInicialBacterias) {
-        this.numeroInicialBacterias = numeroInicialBacterias;
-    }
-
-    public double getTemperatura() {
-        return temperatura;
-    }
-
-    public void setTemperatura(double temperatura) {
-        this.temperatura = temperatura;
-    }
-
-    public String getCondicionLuz() {
-        return condicionLuz;
-    }
-
-    public void setCondicionLuz(String condicionLuz) {
-        this.condicionLuz = condicionLuz;
-    }
-
-    public double getDosisComida() {
-        return dosisComida;
-    }
-
-    public void setDosisComida(double dosisComida) {
-        this.dosisComida = dosisComida;
-    }
-
-    public void setDosisDiaria(int[] dosisDiaria) {
-        this.dosisDiaria = dosisDiaria;
+    public List<PoblacionBacterias> getPoblaciones() {
+        return this.poblaciones;
     }
 }
