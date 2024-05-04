4e25c103ec8b20e45d58b8d9d610c862987f4ca8
jsepucas <jsepucas@myuax.com>
2024-05-04 14:41:06 +0200

    Commits

--- src/main/java/A_Interfaz_Usuario/DialogoAgregarPoblacion.java
+++ src/main/java/A_Interfaz_Usuario/DialogoAgregarPoblacion.java
@@ -7,3 +7,12 @@ import java.awt.event.*;
 public class DialogoAgregarPoblacion extends JDialog {
     private JTextField nombrePoblacionField;
     private JButton guardarBtn, cancelarBtn;
+
+    public DialogoAgregarPoblacion(JFrame owner) {
+        super(owner, "Agregar Población de Bacterias", true);
+        setSize(300, 200);
+        setLayout(new GridLayout(0, 2));
+
+        add(new JLabel("Nombre de la Población:"));
+        nombrePoblacionField = new JTextField();
+        add(nombrePoblacionField);
