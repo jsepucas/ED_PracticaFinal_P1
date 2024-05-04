d7d1850fe05cc62650a6bdeef794b05fa1384f64
jsepucas <jsepucas@myuax.com>
2024-05-04 16:29:02 +0200

    Commitss

--- src/main/java/A_Interfaz_Usuario/DialogoCrearExperimento.java
+++ src/main/java/A_Interfaz_Usuario/DialogoCrearExperimento.java
@@ -5,4 +5,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+public class DialogoCrearExperimento extends JDialog {
+    private JTextField nombreExperimentoField;
+    private JButton guardarBtn, cancelarBtn;
+    private Experimento experimentoActual; // Almacena el experimento actual si está editando
+
+    // Constructor para crear un nuevo experimento
+    public DialogoCrearExperimento(JFrame owner) {
+        this(owner, null); // Llama al constructor sobrecargado con 'null' para el experimento
+    }
 
