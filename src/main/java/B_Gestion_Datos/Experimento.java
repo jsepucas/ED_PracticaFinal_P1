b0d6ad93ca0dd9f57bb45d493321608e6de38da0
jsepucas <jsepucas@myuax.com>
2024-05-04 18:35:40 +0200

    Commitss

--- src/main/java/B_Gestion_Datos/Experimento.java
+++ src/main/java/B_Gestion_Datos/Experimento.java
@@ -12,3 +12,21 @@ public class Experimento {
     private String condicionLuz;
     private double dosisComida;
 
+    // Add getters and setters for the new fields
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getBacteria() {
+        return bacteria;
+    }
+
+    public void setBacteria(String bacteria) {
+        this.bacteria = bacteria;
+    }
+
+
