265f76177e1f9d3517c32caa3e555c3b14000d6a
jsepucas <jsepucas@myuax.com>
2024-05-18 13:07:48 +0200

    Continuación con el proyecto

--- src/main/java/C_Logica_Negocio/SimulacionMontecarlo.java
+++ src/main/java/C_Logica_Negocio/SimulacionMontecarlo.java
@@ -3,3 +3,18 @@ package C_Logica_Negocio;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+public class SimulacionMontecarlo {
+
+    private static final int TAMANO_PLATO = 20;
+    private int[][] comida;
+    private List<Bacteria> bacterias;
+    private Random random;
+
+    public SimulacionMontecarlo(int comidaInicial) {
+        this.comida = new int[TAMANO_PLATO][TAMANO_PLATO];
+        this.bacterias = new ArrayList<>();
+        this.random = new Random();
+        inicializarPlato(comidaInicial);
+        inicializarBacterias();
+    }
