6d8e3a77992180dabe0c78bfb182e07505397017
jsepucas <jsepucas@myuax.com>
2024-05-18 12:48:20 +0200

    Commit final, jar incluido en el proyecto.

--- src/main/java/C_Logica_Negocio/CalculadoraDosisComida.java
+++ src/main/java/C_Logica_Negocio/CalculadoraDosisComida.java
@@ -2,6 +2,9 @@ package C_Logica_Negocio;
 
 public class CalculadoraDosisComida {
 
+    private static final int LIMITE_MAX_COMIDA = 300000; // en microgramos
+
+    // Patrón de incremento y decremento lineal existente
     public static int[] calcularDosisComida(int comidaInicial, int diaPico, int comidaPico, int comidaFinal, int totalDias) {
         int[] dosisDiaria = new int[totalDias];
 
@@ -19,4 +22,35 @@ public class CalculadoraDosisComida {
 
         return dosisDiaria;
     }
+
+    // Nuevo patrón: Cantidad Constante
+    public static int[] calcularDosisConstante(int cantidadFija, int totalDias) {
+        int[] dosisDiaria = new int[totalDias];
+        for (int i = 0; i < totalDias; i++) {
+            dosisDiaria[i] = cantidadFija;
+        }
+        return dosisDiaria;
+    }
+
+    // Nuevo patrón: Incremento Lineal
+    public static int[] calcularIncrementoLineal(int cantidadInicial, int cantidadFinal, int totalDias) {
+        int[] dosisDiaria = new int[totalDias];
+        double incrementoDiario = (cantidadFinal - cantidadInicial) / (double) (totalDias - 1);
+        for (int i = 0; i < totalDias; i++) {
+            dosisDiaria[i] = (int) Math.round(cantidadInicial + i * incrementoDiario);
+        }
+        return dosisDiaria;
+    }
+
+    // Nuevo patrón: Patrón Alternante
+    public static int[] calcularPatronAlternante(int cantidad, int totalDias) {
+        int[] dosisDiaria = new int[totalDias];
+        for (int i = 0; i < totalDias; i++) {
+            dosisDiaria[i] = (i % 2 == 0) ? cantidad : 0;
+        }
+        return dosisDiaria;
+    }
 }
+
+
+
